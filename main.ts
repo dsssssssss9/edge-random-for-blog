34638de58fe25adb58f21f36282c21b11d430948
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-07-19 23:12:22 +0100

    set bright vaRIABLE

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 let strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
 strip.clear()
 strip.show()
+let Delay = 196
 basic.forever(function () {
     for (let index = 0; index <= 9; index++) {
-        strip.setPixelColor(index, neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
+        strip.setPixelColor(index, neopixel.rgb(randint(0, Delay), randint(0, Delay), randint(0, Delay)))
         strip.show()
     }
 })
