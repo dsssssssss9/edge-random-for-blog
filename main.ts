8adcc5cf84efa2b206a2caae0f8489c48935b3cf
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-07-19 23:13:21 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 let strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
 strip.clear()
 strip.show()
-let Delay = 196
+let Bright = 196
 basic.forever(function () {
     for (let index = 0; index <= 9; index++) {
-        strip.setPixelColor(index, neopixel.rgb(randint(0, Delay), randint(0, Delay), randint(0, Delay)))
+        strip.setPixelColor(index, neopixel.rgb(randint(0, Bright), randint(0, Bright), randint(0, Bright)))
         strip.show()
     }
 })
